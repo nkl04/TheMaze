06ffb55b77c4c6facfca0432d2f1e19dab85739e
nkl04 <longnguyenkim2707@gmail.com>
2024-01-30 17:50:25 +0700

    add coyote time vs jump buffer

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,5 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEditor.Tilemaps;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -11,8 +13,28 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private float moveSpeed = 10;
     [SerializeField] private float jumpForce = 10;
 
+    [Header("Improvement")]
+    [SerializeField] private float coyoteTime = 0.2f;
+    [SerializeField] private float jumpBufferTime = 0.2f;
+    
+
+    [Space(5)]
+
+    [Header("Ground Check")]
+    [SerializeField] private bool isGrounded; 
+    [SerializeField] private Transform groundCheckPoint;
+    [SerializeField] private Vector2 groundCheckSize;
+    [SerializeField] private LayerMask groundCheckLayer;
+    
+    
+
     private Rigidbody2D rb2d;
     private Vector2 direction;
+    private bool isFacingRight = true;
+    private bool canJump;
+    private float coyoteTimeCounter;
+    private float jumpBufferTimeCounter;
+
 
     
     private void Start()
@@ -21,8 +43,53 @@ public class PlayerController : MonoBehaviour
     }
 
     private void Update()
-    {
+    {   
+        //horizontal movement
         HorizontalMovement();
+
+
+        //flip player when change direction
+        if((isFacingRight && direction.x < 0) || (!isFacingRight && direction.x > 0))
+        {
+            isFacingRight = !isFacingRight;
+            Flip();
+        } 
+
+        //check if player is grounded
+        isGrounded = IsGrounded();
+
+
+        // Experience Improvement
+        #region  Coyote Time & Jump Buffer 
+        if (isGrounded)
+        {
+            coyoteTimeCounter = coyoteTime;
+        }
+        else
+        {
+            coyoteTimeCounter -= Time.deltaTime;
+        }
+
+        if (coyoteTimeCounter > 0)
+        {
+            canJump = true;
+        }
+        else
+        {
+            canJump = false;
+        }
+
+        jumpBufferTimeCounter -= Time.deltaTime;
+
+        if(jumpBufferTimeCounter > 0 && canJump)
+        {
+            VerticalMovement();
+        }
+
+        #endregion
+
+
+    
     }
 
     private void HorizontalMovement()
@@ -33,9 +100,24 @@ public class PlayerController : MonoBehaviour
 
     private void VerticalMovement()
     {
+        //change the vetical position of player (Jump) 
         rb2d.velocity = new Vector2(rb2d.velocity.x, jumpForce);
     }
 
+    
+
+    public bool IsGrounded()
+    {
+        return Physics2D.OverlapBox(new Vector2(groundCheckPoint.position.x, groundCheckPoint.position.y),groundCheckSize,0,groundCheckLayer);
+    }
+
+    public void Flip()
+    {
+        transform.Rotate(0f,180f,0f);
+    }
+
+
+    //============================================ PLAYER INPUT SYSTEM ================================================
     public void OnMove(InputAction.CallbackContext context)
     {
         direction = context.ReadValue<Vector2>();
@@ -44,8 +126,27 @@ public class PlayerController : MonoBehaviour
     public void OnJump(InputAction.CallbackContext context)
     {
         if (context.performed)
-        {
-            VerticalMovement();
+        {   
+            if(canJump)
+            {
+                VerticalMovement();
+            }
+            else 
+            {
+                //on the air
+                jumpBufferTimeCounter = jumpBufferTime;
+            }
         }
     }
+
+    //=================================================================================================================
+
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireCube(groundCheckPoint.position,groundCheckSize);
+    }
+
+    
 }
