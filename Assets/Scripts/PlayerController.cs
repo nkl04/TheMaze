3fdee562fe2d6351a98eba29a42bda9984068231
MinhAnhNek <nguyenminan20001011@gmail.com>
2024-02-23 21:57:07 +0700

    AnimationMau
    action jump

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
+using UnityEditor.Tilemaps;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
@@ -8,6 +9,7 @@ public class PlayerController : MonoBehaviour
     private const string IS_RUNNING = "isRunning";
     [SerializeField] private float speed = 10f; 
     [SerializeField] private Animator animator;
+    [SerializeField] private float height;
     private Rigidbody2D rb2D;
 
     public void Start ()
@@ -15,6 +17,8 @@ public class PlayerController : MonoBehaviour
         rb2D = GetComponent<Rigidbody2D>();
     }
     private float direction;
+    
+    private float up_down;
     private bool isFacingRight = true;
     
     public void Update() 
@@ -23,6 +27,8 @@ public class PlayerController : MonoBehaviour
     }
     private void Movement(){
         direction = Input.GetAxisRaw("Horizontal");
+
+        up_down = Input.GetAxisRaw("Vertical");
         
         rb2D.velocity = new Vector2(speed*direction, rb2D.velocity.y);
 
@@ -33,6 +39,11 @@ public class PlayerController : MonoBehaviour
             Flip();
         }
         animator.SetBool(IS_RUNNING, direction != 0);
+
+        if(Input.GetKeyDown(KeyCode.UpArrow) == true)
+        {
+            rb2D.velocity = Vector2.up * height;
+        }
         
     }
     private void Flip() 
