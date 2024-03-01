c1504ee21b7c7fc30095a1f34ed4732c7041e006
nkl04 <longnguyenkim2707@gmail.com>
2024-03-02 00:42:55 +0700

    feat: demo door open
    trigger with button: open door
    exit button: close door

--- Assets/Scripts/Elevator/Button.cs
+++ Assets/Scripts/Elevator/Button.cs
@@ -4,20 +4,49 @@ using UnityEngine;
 
 public class Button : MonoBehaviour
 {
+
+    public enum Open{
+        Elevator,
+        Door
+    }
+
     [SerializeField] private Elevator[] elevatorArray;
+    [SerializeField] private Door[] doorArray;
+    [SerializeField] Open open;
 
     private void OnTriggerEnter2D(Collider2D other) {
-        foreach (Elevator elevator in elevatorArray)
+        if (open == Open.Elevator)
         {
-            elevator.IsTurnOn = true; 
+            foreach (Elevator elevator in elevatorArray)
+            {
+                elevator.IsTurnOn = true; 
+            }
         }
+        else
+        {
+            foreach (Door door in doorArray)
+            {
+                door.IsTurnOn = true; 
+            }
+        }
+        
          
     }
 
     private void OnTriggerExit2D(Collider2D other) {
-        foreach (Elevator elevator in elevatorArray)
+        if (open == Open.Elevator)
         {
-            elevator.IsTurnOn = false; 
+            foreach (Elevator elevator in elevatorArray)
+            {
+                elevator.IsTurnOn = false; 
+            }
+        }
+        else
+        {
+            foreach (Door door in doorArray)
+            {
+                door.IsTurnOn = false; 
+            }
         }
     }
     
