75954067cbf00cd875c2ac66db55ad9d89375dcb
MinhAnhNek <nguyenminan20001011@gmail.com>
2024-03-07 00:04:49 +0700

    pause system UI
    background

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,8 +7,7 @@ using UnityEngine.Events;
 
 public class PlayerController : MonoBehaviour
 {
-    private const string IS_RUNNING = "isRunning";
-    private const string VECTOR_Y = "Vector y";
+    private const string SPEED = "Speed";
     private const string IS_JUMPING = "isJumping";
     private const string IS_TOUCHING_GROUND = "isTouchingGround";
     private const string IS_TOUCHING_WALL = "isTouchingWall";
@@ -52,7 +51,7 @@ public class PlayerController : MonoBehaviour
         
         animator.SetBool(IS_TOUCHING_WALL, isTouchingWall);
         
-        animator.SetBool(IS_RUNNING, direction != 0);
+        animator.SetFloat(SPEED, direction*rb2D.velocity.x);
 
         if(Input.GetButtonDown("Jump") && isTouchingGround == true)
         {   
@@ -67,16 +66,17 @@ public class PlayerController : MonoBehaviour
             if(Input.GetButtonDown("Jump")) 
             {
                 animator.SetBool(IS_JUMPING, true);
-                animator.SetBool(IS_RUNNING, false); 
+                //animator.SetBool(IS_RUNNING, false); 
                 
             }
-            else animator.SetBool(IS_RUNNING, true);
+            //else animator.SetBool(IS_RUNNING, true);
 
             if (isTouchingWall == true && direction != 0)
             {
                 direction = 0;
-                rb2D.velocity = new Vector2(rb2D.velocity.x, -1);
-                animator.SetBool(IS_RUNNING, false);
+                //animator.SetFloat(SPEED, 0);
+                rb2D.velocity = new Vector2(0, -5f);
+                //animator.SetBool(IS_RUNNING, false);
             }
             
             isFacingRight = !isFacingRight;
