fa0f39a589e9a09793572c8b904aa204db60d169
MinhAnhNek <nguyenminan20001011@gmail.com>
2024-03-13 21:05:30 +0700

    Delete

--- Assets/Scripts/Attackable/IAttackable.cs
+++
@@ -1,8 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public abstract class AbstractAttackable : MonoBehaviour
-{
-    public abstract void OnTriggerEnter2D(Collider2D other);
-}
